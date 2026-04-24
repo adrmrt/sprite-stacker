e2009f864e935a28cb130b43f8d6ce88acd5bfcd
adrmrt <adrian.e.martinez.ayuso@gmail.com>
2026-04-24 23:29:04 +0200

    chore: updated

--- src/main.py
+++ src/main.py
@@ -44,7 +44,7 @@ class SpriteStacker(tk.Tk):
         self.split_container.pack(fill=tk.BOTH, expand=True)
 
     def create_tree_view(self):
-        self.tree_view = ttk.Treeview(self.split_container, show='tree')
+        self.tree_view = ttk.Treeview(self.split_container, show="tree")
         self.tree_view.tag_configure("disabled", foreground="gray")
         self.split_container.add(self.tree_view)
 
@@ -53,33 +53,37 @@ class SpriteStacker(tk.Tk):
         self.split_container.add(self.preview_frame)
 
     def bind_events(self):
-        self.tree_view.bind('<Button-1>', self.on_drag_start)
-        self.tree_view.bind('<B1-Motion>', self.on_drag_motion)
-        self.tree_view.bind('<ButtonRelease-1>', self.on_drag_release)
+        self.tree_view.bind("<Button-1>", self.on_drag_start)
+        self.tree_view.bind("<B1-Motion>", self.on_drag_motion)
+        self.tree_view.bind("<ButtonRelease-1>", self.on_drag_release)
 
         self.tree_view.bind("<Delete>", self.on_del_key)
 
         self.tree_view.bind("<Double-1>", self.on_double_click)
 
-        self.bind('<Configure>', self.on_resize)
+        self.bind("<Configure>", self.on_resize)
 
         self.tree_view.bind("<Button-3>", self.show_context_menu)
         self.create_context_menu()
 
     def create_context_menu(self):
         self.tree_view_menu = tk.Menu(self, tearoff=0)
-        self.tree_view_menu.add_command(label="Remove", command=self.remove_selected_items)
+        self.tree_view_menu.add_command(
+            label="Remove", command=self.remove_selected_items
+        )
 
     def on_drop(self, filenames):
         for file in filenames:
-            file = file.decode('utf-8')
-            if os.path.isfile(file) and file.lower().endswith(('.png', '.jpg')):
+            file = file.decode("utf-8")
+            if os.path.isfile(file) and file.lower().endswith((".png", ".jpg")):
                 self.insert_image(file)
         self.load_and_display_images()
 
     def open_images(self):
         file_types = [("Images", ".png;*.jpg")]
-        selected_files = filedialog.askopenfilenames(title="Select images", filetypes=file_types)
+        selected_files = filedialog.askopenfilenames(
+            title="Select images", filetypes=file_types
+        )
         for file in reversed(selected_files):
             self.insert_image(file)
         self.load_and_display_images()
@@ -88,12 +92,12 @@ class SpriteStacker(tk.Tk):
         self.image_paths.insert(0, image)
         filename = os.path.basename(image)
         self.image_enabled[filename] = True
-        self.tree_view.insert('', 0, text=filename)
+        self.tree_view.insert("", 0, text=filename)
 
     def on_double_click(self, event):
         item = self.tree_view.identify_row(event.y)
         if item:
-            filename = self.tree_view.item(item, 'text')
+            filename = self.tree_view.item(item, "text")
             is_enabled = self.image_enabled.get(filename, True)
             self.image_enabled[filename] = not is_enabled
 
@@ -105,22 +109,24 @@ class SpriteStacker(tk.Tk):
             self.load_and_display_images()
 
     def save_image(self):
-        if not hasattr(self, 'current_image') or self.current_image is None:
+        if not hasattr(self, "current_image") or self.current_image is None:
             print("No combined image to save.")
             return
 
         file_types = [("PNG files", "*.png")]
-        file_path = filedialog.asksaveasfilename(title="Save image as", filetypes=file_types, defaultextension=".png")
+        file_path = filedialog.asksaveasfilename(
+            title="Save image as", filetypes=file_types, defaultextension=".png"
+        )
 
         if file_path:
             try:
-                self.current_image.save(file_path, format='PNG')
+                self.current_image.save(file_path, format="PNG")
                 print(f"Image successfully saved to {file_path}")
             except Exception as e:
                 print(f"Error saving image: {e}")
 
     def on_resize(self, event):
-        if hasattr(self, 'image') and self.image:
+        if hasattr(self, "image") and self.image:
             self.load_and_display_images()
 
     def load_and_display_images(self):
@@ -128,7 +134,9 @@ class SpriteStacker(tk.Tk):
         preview_max_height = self.preview_frame.winfo_height()
 
         if not self.image_paths:
-            placeholder_image = Image.new('RGBA', (preview_max_width, preview_max_height), (255, 255, 255, 0))
+            placeholder_image = Image.new(
+                "RGBA", (preview_max_width, preview_max_height), (255, 255, 255, 0)
+            )
             self.update_image_display(placeholder_image)
         else:
             max_width = 0
@@ -151,28 +159,34 @@ class SpriteStacker(tk.Tk):
                 max_width = max(max_width, img.width)
                 max_height = max(max_height, img.height)
 
-            combined_image = Image.new('RGBA', (max_width, max_height), (0, 0, 0, 0))  # Background is transparent
+            combined_image = Image.new(
+                "RGBA", (max_width, max_height), (0, 0, 0, 0)
+            )  # Background is transparent
 
             for img in images:
                 x_offset = (max_width - img.width) // 2
                 y_offset = (max_height - img.height) // 2
-                temp_image = Image.new('RGBA', (max_width, max_height), (0, 0, 0, 0))
-                temp_image.paste(img, (x_offset, y_offset), img if img.mode == 'RGBA' else None)
+                temp_image = Image.new("RGBA", (max_width, max_height), (0, 0, 0, 0))
+                temp_image.paste(
+                    img, (x_offset, y_offset), img if img.mode == "RGBA" else None
+                )
                 combined_image.alpha_composite(temp_image)
 
             if max_width > preview_max_width or max_height > preview_max_height:
-                combined_image.thumbnail((preview_max_width, preview_max_height), Image.LANCZOS)
+                combined_image.thumbnail(
+                    (preview_max_width, preview_max_height), Image.LANCZOS
+                )
 
             self.update_image_display(combined_image)
             self.current_image = combined_image
 
     def update_image_display(self, image):
         self.image = ImageTk.PhotoImage(image)
-        if hasattr(self, 'preview_label'):
+        if hasattr(self, "preview_label"):
             self.preview_label.configure(image=self.image)
         else:
             self.preview_label = tk.Label(self.preview_frame, image=self.image)
-            self.preview_label.pack(expand=True, anchor='center')
+            self.preview_label.pack(expand=True, anchor="center")
 
     def show_context_menu(self, event):
         try:
@@ -193,7 +207,11 @@ class SpriteStacker(tk.Tk):
 
         item = self.tree_view.identify_row(event.y)
         if item and item != self.dragged_item:
-            self.tree_view.move(self.dragged_item, self.tree_view.parent(item), self.tree_view.index(item))
+            self.tree_view.move(
+                self.dragged_item,
+                self.tree_view.parent(item),
+                self.tree_view.index(item),
+            )
 
     def on_drag_release(self, event):
         self.dragged_item = None
@@ -203,7 +221,7 @@ class SpriteStacker(tk.Tk):
     def update_image_order(self):
         new_image_paths = []
         for item in self.tree_view.get_children():
-            filename = self.tree_view.item(item, 'text')
+            filename = self.tree_view.item(item, "text")
             for path in self.image_paths:
                 if os.path.basename(path) == filename:
                     new_image_paths.append(path)
@@ -219,7 +237,7 @@ class SpriteStacker(tk.Tk):
             return
 
         for selected_item in selected_items:
-            filename = self.tree_view.item(selected_item, 'text')
+            filename = self.tree_view.item(selected_item, "text")
             self.tree_view.delete(selected_item)
             self.update_image_lists_and_states(filename)
 
